dfd3805d30e21b9d4c77a3c76e045439013cf671
snaplemouton <maxime.caron.cem@gmail.com>
2015-01-24 03:40:23 -0500

    Main Character + NPC
    Movement of main character
    NPC class
    Collider between Character and NPC
    - Need to ensure that pressing the spacebar call the good GUI methods.
    - Place objects on the scene.
    - Add the affection float to the NPC class.

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NPC : MonoBehaviour {
+	[SerializeField]
+	private int m_npcId;
+	public int npcId
+	{
+		get {return m_npcId; }
+	}
+
+	[SerializeField]
+	private int m_dialogId;
+	public int dialogId
+	{
+		get {return m_dialogId; }
+		set {m_dialogId = value; }
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+	public float moveSpeed = 6.0F;
+	public float gravity = 20.0F;
+	public float turnSpeed = 100.0F;
+	public float colliderDistance = 1.2F;
+	private Vector3 moveDirection = Vector3.zero;
+	private bool isCollided = false;
+
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		//Feed moveDirection with input.
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		moveDirection = transform.TransformDirection(moveDirection);
+
+		if(Input.GetKey(KeyCode.LeftArrow))
+			transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+		if(Input.GetKey(KeyCode.RightArrow))
+			transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+
+		//Verify if the player is in range of an NPC.
+		RaycastHit hit;
+		if(Physics.Raycast(transform.position, moveDirection, out hit, colliderDistance)) {
+			if(hit.collider.tag == "NPC") {
+				if(!isCollided) {
+					isCollided = true;
+					NPC npc = hit.collider.transform.gameObject.GetComponent<NPC>();
+					Debug.Log(npc.npcId);
+					//GUI.ShowColliderMessage = true;
+					//if(Input.GetKey(KeyCode.Space))
+						//GUI.SetNPC(npc.npcId);
+				}
+			}
+			else isCollided = false;
+		}
+		else isCollided = false;
+		//Multiply it by speed.
+		moveDirection *= moveSpeed;
+		//Applying gravity to the controller
+		moveDirection.y -= gravity * Time.deltaTime;
+		//Making the character move
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+
+
+}
